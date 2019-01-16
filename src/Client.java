babe4caa2b2a3f7e3e4705baf1433dfb5163b90a
bourakkadi.reda@gmail.com <Redazebest1996/>
2019-01-16 10:09:03 +0100

    commit reda

--- src/Client.java
+++ src/Client.java
@@ -10,32 +10,37 @@ public class Client {
     public static void main(String[] args) {
         try {
 
-            ServeurDonjon serveurDonjon = (ServeurDonjon) Naming.lookup("//localhost/serveurDonjon");
+            //ServeurDonjon serveurDonjon = (ServeurDonjon) Naming.lookup("//localhost/serveurDonjon");
             ServeurDiscussion serveurDiscussion = (ServeurDiscussion) Naming.lookup("//localhost/serveurDiscussion");
 
-            Personnage personnage = serveurDonjon.seConnecter("Reda");
-            System.out.println("Personnage " + personnage.getNomPersonnage() +" connecté au serveur de jeu");
-            serveurDiscussion.seConnecter(personnage);
-            System.out.println("Personnage " + personnage.getNomPersonnage() +" connecté au serveur de discussion");
+            //Personnage personnage = serveurDonjon.seConnecter("Reda");
+            //System.out.println("Personnage " + personnage.getNomPersonnage() +" connecté au serveur de jeu");
+            //serveurDiscussion.seConnecter(personnage);
+            //System.out.println("Personnage " + personnage.getNomPersonnage() +" connecté au serveur de discussion");
 
             System.out.println("Veuillez entrer la direction dans laquelle vous souhaitez vous diriger");
 
             Scanner scanner = new Scanner(System.in);
-            String texteClient = scanner.next();
+            String texteClient;
 
-            if ( texteClient.startsWith("\"") && texteClient.endsWith("\"")){
-                serveurDiscussion.discuter(personnage,texteClient);
+            boolean outOfLoop;
+
+            do {
+                texteClient = scanner.next();
+                outOfLoop = true;
+                if (texteClient.startsWith("\"") && texteClient.endsWith("\"")) {
+                    //serveurDiscussion.discuter(personnage, texteClient);
+                    System.out.println("Good entry");
+                    outOfLoop = false;
+                } else if (texteClient.equals("N") || texteClient.equals("S") || texteClient.equals("O") || texteClient.equals("E")) {
+                    //serveurDonjon.seDeplacer(personnage, texteClient);
+                    System.out.println("Also good entry");
+                    outOfLoop = false;
+                } else
+                    System.out.println("Veuillez entrer une direction ou un message");
             }
-            else if( texteClient.equals("N") || texteClient.equals("S") || texteClient.equals("O") || texteClient.equals("E")  ){
-                serveurDonjon.seDeplacer(personnage,texteClient);
-            }
-            else
-                System.out.println("Veuillez entrer une direction ou un message");
+            while( outOfLoop == true );
 
-
-
-
-            serveurDonjon.seDeplacer(personnage,);
 
 
         }catch (Exception e){
--- src/ServeurDiscussion.java
+++ src/ServeurDiscussion.java
@@ -1,12 +1,15 @@
 import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public interface ServeurDiscussion extends Remote {
 
-    void discuter(Personnage personnageEmetteur, String message);
 
-    void seConnecter(Personnage personnage);
+    void discuter(Personnage personnageEmetteur, String message) throws RemoteException;
 
-    void seDeconnecter(Personnage personnage);
+    void seConnecter(Personnage personnage) throws RemoteException;
 
+    void seDeconnecter(Personnage personnage) throws RemoteException;
 
 }
