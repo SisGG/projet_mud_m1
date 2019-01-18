f785ec589d5dc22b488cef9737ce24bd2df75a05
LanXor <Thomas.olivier3@yahoo.fr>
2019-01-18 12:39:38 +0100

    Implementation serveurDiscussion.

--- src/Systeme.java
+++ src/Systeme.java
@@ -5,22 +5,36 @@ public class Systeme {
 
     private int tailleDonjon = 5;
     private ServeurDonjonImpl serveurDonjon;
+    private ServeurDiscussionImpl serveurDiscussion;
 
-    public void lancerServeurDiscussion() { }
+    public void lancerServeurDiscussion() {
+        try {
+            this.serveurDiscussion = new ServeurDiscussionImpl();
+            Naming.rebind("ServeurDiscussion", this.serveurDiscussion);
+            System.out.println("Serveur discussion déclaré.");
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
+    }
 
     private void lancerServeurDonjon() {
         try {
-            LocateRegistry.createRegistry(1099);
             this.serveurDonjon = new ServeurDonjonImpl(this.tailleDonjon);
             Naming.rebind("ServeurDonjon", this.serveurDonjon);
             System.out.println("Serveur donjon déclaré.");
-        } catch(Exception e) {
+        } catch ( Exception e ) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
         Systeme systeme = new Systeme();
+        try {
+            LocateRegistry.createRegistry(1099);
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
         systeme.lancerServeurDonjon();
+        systeme.lancerServeurDiscussion();
     }
 }
