50dae62fd65fd2bd56bfb29dbbe525423fdbd9b1
LanXor <Thomas.olivier3@yahoo.fr>
2019-01-18 11:46:02 +0100

    Commentaire.

--- src/Personnage.java
+++ src/Personnage.java
@@ -5,6 +5,10 @@ public class Personnage implements Serializable {
     private String nomPersonnage;
     private Piece pieceActuelle;
 
+    /**
+     * Constructeur de la classe Personnage.
+     * @param nomPersonnage Nom du personnage.
+     */
     public Personnage(String nomPersonnage) {
         this.nomPersonnage = nomPersonnage;
         this.pieceActuelle = null;
@@ -14,14 +18,26 @@ public class Personnage implements Serializable {
         this.pieceActuelle = pieceActuelle;
     }
 
+    /**
+     * Récupère la piece actuelle du personnage.
+     * @return Renvoie la piece du personnage.
+     */
     public Piece getPieceActuelle() {
         return this.pieceActuelle;
     }
 
+    /**
+     * Récupère le nom du personnage.
+     * @return Renvoie la chaine de caractère du nom de personnage.
+     */
     public String getNomPersonnage() {
         return this.nomPersonnage;
     }
 
+    /**
+     * Récupère la chaine de caractère définissant l'objet.
+     * @return Renvoie une chaine de caractère.
+     */
     public String toString() {
         return "Personnage[" + this.nomPersonnage + "]";
     }
