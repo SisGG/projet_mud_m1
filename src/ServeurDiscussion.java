d21a0d1c3b3b6e7fa115ad780503d55e087f1da7
LanXor <Thomas.olivier3@yahoo.fr>
2019-01-22 09:46:36 +0100

    Ajout des cartouches fichier.

--- src/ServeurDiscussion.java
+++ src/ServeurDiscussion.java
@@ -1,3 +1,19 @@
+/* ****************************************************************************
+ *
+ * Name File : src/ServeurDiscussion.java
+ * Authors   : OLIVIER Thomas
+ *             BOURAKADI Reda
+ *             LAPEYRADE Sylvain
+ *
+ * Location  : UPSSITECH - University Paul Sabatier
+ * Date      : Janvier 2019
+ *
+ *                        This work is licensed under a
+ *              Creative Commons Attribution 4.0 International License.
+ *                                    (CC BY)
+ *
+ * ***************************************************************************/
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.HashMap;
--- src/ServeurNotificationImpl.java
+++ src/ServeurNotificationImpl.java
@@ -1,3 +1,19 @@
+/* ****************************************************************************
+ *
+ * Name File : src/ServeurNotificationImpl.java
+ * Authors   : OLIVIER Thomas
+ *             BOURAKADI Reda
+ *             LAPEYRADE Sylvain
+ *
+ * Location  : UPSSITECH - University Paul Sabatier
+ * Date      : Janvier 2019
+ *
+ *                        This work is licensed under a
+ *              Creative Commons Attribution 4.0 International License.
+ *                                    (CC BY)
+ *
+ * ***************************************************************************/
+
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
