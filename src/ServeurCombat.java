e00a2ae9d7f4b7df58e98b67746a5397053ff033
LanXor <Thomas.olivier3@yahoo.fr>
2019-02-06 22:16:54 +0100

    Modification des actions

--- src/ServeurCombat.java
+++ src/ServeurCombat.java
@@ -16,13 +16,6 @@ import java.util.HashMap;
  *****************************************************************************/
 public interface ServeurCombat extends Remote {
 
-    HashMap<String, EtreVivant> LancerCombat(Personnage personnage) throws RemoteException;
+    void lancerCombat(Personnage personnage) throws RemoteException;
 
-    void seConnecter(EtreVivant etreVivant) throws RemoteException;
-
-    void seDeconnecter(EtreVivant etreVivant) throws  RemoteException;
-
-    void enregistrerNotification(Personnage personnage, ServeurNotification serveurNotification) throws RemoteException;
-
-    void enleverNotification(Personnage personnage) throws RemoteException;
 }
