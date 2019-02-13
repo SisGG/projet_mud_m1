d27be30e6c6bedeb00023984887a417dac601bae
LanXor <Thomas.olivier3@yahoo.fr>
2019-02-13 10:18:32 +0100

    Commentaires

--- src/ServeurCombatImpl.java
+++ src/ServeurCombatImpl.java
@@ -17,11 +17,22 @@ public class ServeurCombatImpl extends UnicastRemoteObject implements ServeurCom
 
     private Donjon donjon;
 
+    /**
+     * Constructeur de la classe ServeurCombatImpl.
+     * @param donjon Base de données Donjon pour le serveur.
+     * @throws RemoteException Exception déclenchée si ServeurCombatImpl ne crée pas l'objet.
+     */
     ServeurCombatImpl(Donjon donjon) throws RemoteException {
         super();
         this.donjon = donjon;
     }
 
+    /**
+     * Permet de lancer un combat entre un Personnage et un Monstre.
+     * @param personnage Personnage qui est attaquer par un Monstre.
+     * @return Renvoie la valeur true si le personnage attaqué est mort, false sinon.
+     * @throws RemoteException Exception déclenchée si la méthode n'est pas invoquée.
+     */
     public boolean lancerCombatMonstre(Personnage personnage) throws RemoteException {
         Monstre monstre = new Monstre(personnage.getPieceActuelle());
         while ( this.donjon.nomEtreVivantExist(monstre.getNom()) ) {
@@ -42,6 +53,10 @@ public class ServeurCombatImpl extends UnicastRemoteObject implements ServeurCom
         return (etreVivantSortant.equals(personnage) && etreVivantSortant.getPointDeVie()==0);
     }
 
+    /**
+     * Fais regagner tous les points de vie à tous les EtreVivants se trouvant dans la Pièce passée en paramètre.
+     * @param piece Pièce dont tous les EtreVivants doivent regagner tous leurs points de vie.
+     */
     private void regagnerVieMax(Piece piece){
         if ( !this.donjon.seDeroulerCombatPiece(piece) ) {
             for ( EtreVivant etreVivant : this.donjon.getEtreVivantMemePiece(piece) ) {
