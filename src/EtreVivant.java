3d548c8f040a633ea074ae0ca0cbeee7903af0e3
LanXor <Thomas.olivier3@yahoo.fr>
2019-02-13 10:29:19 +0100

    Commentaires cartouches

--- src/EtreVivant.java
+++ src/EtreVivant.java
@@ -5,9 +5,9 @@ import java.io.Serializable;
  * @author  : OLIVIER Thomas
  *            BOURAKADI Reda
  *            LAPEYRADE Sylvain
- * @version : 1.0
+ * @version : 2.0
  * location : UPSSITECH - University Paul Sabatier
- * date     : 30 Janvier 2019
+ * date     : 18 Février 2019
  * licence  :              This work is licensed under a
  *              Creative Commons Attribution 4.0 International License.
  *                                    (CC BY)
--- src/ServeurNotificationImpl.java
+++ src/ServeurNotificationImpl.java
@@ -7,9 +7,9 @@ import java.util.Scanner;
  * @author  : OLIVIER Thomas
  *            BOURAKADI Reda
  *            LAPEYRADE Sylvain
- * @version : 1.0
+ * @version : 2.0
  * location : UPSSITECH - University Paul Sabatier
- * date     : 30 Janvier 2019
+ * date     : 18 Février 2019
  * licence  :              This work is licensed under a
  *              Creative Commons Attribution 4.0 International License.
  *                                    (CC BY)
