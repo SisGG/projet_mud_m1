9d76c6950c2701031bf120c275288a9d90cfffb1
LanXor <Thomas.olivier3@yahoo.fr>
2019-03-21 18:22:18 +0100

    Modification pour étape 4.

---
+++ src/ServeurSysteme.java
@@ -0,0 +1,17 @@
+import java.rmi.Remote;
+
+/******************************************************************************
+ * file     : src/ServeurSysteme.java
+ * @author  : OLIVIER Thomas
+ *            BOURAKADI Reda
+ *            LAPEYRADE Sylvain
+ * @version : 3.0
+ * location : UPSSITECH - University Paul Sabatier
+ * date     : 18 Mars 2019
+ * licence  :              This work is licensed under a
+ *              Creative Commons Attribution 4.0 International License.
+ *                                    (CC BY)
+ *****************************************************************************/
+public interface ServeurSysteme extends Remote {
+
+}
--- src/ServeurSystemeImpl.java
+++ src/ServeurSystemeImpl.java
@@ -1,8 +1,10 @@
 import java.rmi.Naming;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
 
 /******************************************************************************
- * file     : src/Systeme.java
+ * file     : src/ServeurSystemeImpl.java
  * @author  : OLIVIER Thomas
  *            BOURAKADI Reda
  *            LAPEYRADE Sylvain
@@ -13,17 +15,18 @@ import java.rmi.registry.LocateRegistry;
  *              Creative Commons Attribution 4.0 International License.
  *                                    (CC BY)
  *****************************************************************************/
-public class Systeme {
+public class ServeurSystemeImpl extends UnicastRemoteObject implements ServeurSysteme {
 
     private static final int tailleDonjon = 5;
     private Donjon donjon;
     private static final BaseDeDonnees baseDeDonnees = new BDFile("DataBase.data");
 
     /**
-     * Constructeur de la classe Systeme.
+     * Constructeur de la classe ServeurSystemeImpl.
      * @param donjon Base de données d'un donjon commun à tous les serveurs.
      */
-    private Systeme(Donjon donjon) {
+    private ServeurSystemeImpl(Donjon donjon) throws RemoteException {
+        super();
         this.donjon = donjon;
         try {
             LocateRegistry.createRegistry(1099);
@@ -90,10 +93,14 @@ public class Systeme {
 
     public static void main(String[] args) {
         Donjon donjon = new Donjon(tailleDonjon);
-        Systeme systeme = new Systeme(donjon);
-        systeme.lancerServeurDonjon();
-        systeme.lancerServeurDiscussion();
-        systeme.lancerServeurCombat();
-        systeme.lancerServeurPersistance();
+        try {
+            ServeurSystemeImpl systeme = new ServeurSystemeImpl(donjon);
+            systeme.lancerServeurDonjon();
+            systeme.lancerServeurDiscussion();
+            systeme.lancerServeurCombat();
+            systeme.lancerServeurPersistance();
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
     }
 }
